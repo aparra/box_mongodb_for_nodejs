339419b48660f9a981dc9a54c4a431cc0e3edbea
Anderson Parra <ander.pp@gmail.com>
2015-08-06 22:31:28 +0100

    add suport script provision

---
+++ scripts/mongodb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+apt-get update
+apt-get install -y mongodb-org
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
---
+++ scripts/provision.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo /vagrant/scripts/nodejs.sh
+sudo /vagrant/scripts/mongodb.sh
