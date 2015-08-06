c2fa97eeaa7679382466a8dbf88099836d27c480
Anderson Parra <ander.pp@gmail.com>
2015-08-06 22:28:29 +0100

    remove chef suport

--- chef/cookbooks/mongodb_for_nodejs/recipes/nodejs.rb
+++
@@ -1,3 +0,0 @@
-%w{ nodejs npm build-essential }.each do |lib|
-  package lib
-end
--- chef/roles/mongodb_for_nodejs.rb
+++
@@ -1,7 +0,0 @@
-name "mongodb_for_nodejs"
-
-run_list(
-  "recipe[apt]",
-  "recipe[mongodb_for_nodejs::bootstrap]",
-  "recipe[mongodb_for_nodejs::nodejs]"
-)
