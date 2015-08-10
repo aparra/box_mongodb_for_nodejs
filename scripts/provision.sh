d69f6a0036ea08bb115565f388144e8704205d8c
Anderson Parra <ander.pp@gmail.com>
2015-08-10 20:35:59 +0100

    add recipe for git and workspace (clone repos)

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+sudo /vagrant/scripts/bootstrap.sh
 sudo /vagrant/scripts/nodejs.sh
 sudo /vagrant/scripts/mongodb.sh
+sudo /vagrant/scripts/workspace.sh
---
+++ scripts/workspace.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+WORKSPACE=/vmspace/workspace
+mkdir -p $WORKSPACE
+cd $WORKSPACE
+git clone https://github.com/aparra/labpad.git
